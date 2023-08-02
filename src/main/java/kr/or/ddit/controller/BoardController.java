0f4271d4e19e2031465815d74f1ec3d9393177c0
ChoiDazzi <choidazzi@gmail.com>
2023-08-02 17:22:03 +0900

    update

--- src/main/java/kr/or/ddit/controller/BoardController.java
+++ src/main/java/kr/or/ddit/controller/BoardController.java
@@ -3,6 +3,7 @@ package kr.or.ddit.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -21,8 +22,17 @@ public class BoardController {
         log.info("modifyForm");
     }
 
-    @GetMapping("/read")
-    public void read(){
-        log.info("read");
+    //board테이블의 12번글 (/board/read/12)
+    //boardNo: 경로 변수: path variable
+    @GetMapping("/read/{no}")
+    public String read(@PathVariable("no") int boardNo){
+        log.info("no={}", boardNo);
+
+        return "board/read";
+    }
+
+    @GetMapping("/update/{no}")
+    public String update(@PathVariable("no") int boardNo) {
+        return "board/update";
     }
 }
