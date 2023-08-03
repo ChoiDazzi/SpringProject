5e1b59c1931488db00a921c24c98ba2b77dca13e
ChoiDazzi <choidazzi@gmail.com>
2023-08-03 09:21:10 +0900

    register.jsp

--- src/main/java/kr/or/ddit/controller/BoardController.java
+++ src/main/java/kr/or/ddit/controller/BoardController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -31,8 +32,20 @@ public class BoardController {
         return "board/read";
     }
 
-    @GetMapping("/update/{no}")
-    public String update(@PathVariable("no") int boardNo) {
+    @GetMapping("/update/{boardNo}")
+    public String update(@PathVariable int boardNo) {
         return "board/update";
+    }
+
+    @GetMapping(value = "/get", params = "register")
+    public String register() {
+        log.info("register");
+        return "board/register";
+    }
+
+    @PostMapping(value = "/post", params = "register")
+    public String registerPost(){
+
+            return "board/register";
     }
 }
--- src/main/webapp/WEB-INF/views/board/register.jsp
+++ src/main/webapp/WEB-INF/views/board/register.jsp
@@ -1,3 +1,12 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <h3>register</h3>
+<p>
+<form action="/board/post" method="post">
+    <input type="text" name="bookId" value="ISBN1234" />
+    <button type="submit" name="register">REGISTER</button>
+    <button type="submit" name="update">UPDATE</button>
+    <button type="submit" name="delete">DELETE</button>
+    <button type="submit" name="list">LIST</button>
+</form>
+</p>
