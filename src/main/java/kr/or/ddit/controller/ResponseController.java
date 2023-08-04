f7926543fecdf315e7e89be7f5eb6dee88fd4d1f
ChoiDazzi <choidazzi@gmail.com>
2023-08-04 10:40:45 +0900

    return String

--- src/main/java/kr/or/ddit/controller/ResponseController.java
+++ src/main/java/kr/or/ddit/controller/ResponseController.java
@@ -1,8 +1,37 @@
 package kr.or.ddit.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
+@RequestMapping("/resp")
 public class ResponseController {
+    //void : 호출하는 URL과 동일한 뷰 이름을 나타내기 위해 사용
+    @GetMapping("/goHome")
+    public void home() {
+        log.info("home");
+    }
 
+    @GetMapping("/goHomeTwo")
+    public void homeTwo() {
+        log.info("homeTwo");
+    }
+    //URL경로
+    @GetMapping("/str")
+    public String str() {
+        log.info("str");
+
+        //파일 경로
+        return "resp/str";
+    }
+
+    @GetMapping("/strTwo")
+    public String strTwo() {
+        log.info("strTwo");
+
+        return "resp/strTwo";
+    }
 }
