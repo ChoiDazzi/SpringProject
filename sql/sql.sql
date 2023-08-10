f972ec51f80aa7fb681df6c09c5c8d0aab14971f
ChoiDazzi <choidazzi@gmail.com>
2023-08-10 09:51:35 +0900

    itemRegister

--- sql/sql.sql
+++ sql/sql.sql
@@ -136,6 +136,16 @@ CREATE TABLE ITEM_ATTACH (
                                  REFERENCES ITEM3(ITEM_ID)
 );
 
+SELECT COLUMN_NAME
+     , DATA_TYPE
+     , CASE WHEN DATA_TYPE='NUMBER' THEN 'private int ' || FN_GETCAMEL(COLUMN_NAME) || ';'
+            WHEN DATA_TYPE IN('VARCHAR2','CHAR') THEN 'private String ' || FN_GETCAMEL(COLUMN_NAME) || ';'
+            WHEN DATA_TYPE='DATE' THEN 'private Date ' || FN_GETCAMEL(COLUMN_NAME) || ';'
+            ELSE 'private String ' || FN_GETCAMEL(COLUMN_NAME) || ';'
+    END AS CAMEL_CASE
+     , '<result property="'||FN_GETCAMEL(COLUMN_NAME)||'" column="'||COLUMN_NAME||'"/>' RESULTMAP
+FROM ALL_TAB_COLUMNS
+WHERE TABLE_NAME = 'ITEM2';
 --------------
 select * from book_info;
 select * from attach;
